3f8a003d2321815dbbd4ca9f9219e4bcc1bab655
Gilles TOURREAU <gilles@tourreau.fr>
2021-01-16 00:58:22 +0100

    Fix bug with scalar values (#7)
    Co-authored-by: Gilles TOURREAU <gilles.tourreau@pos-informatique.fr>

--- MSBuild.Yaml/YamlToJson.cs
+++ MSBuild.Yaml/YamlToJson.cs
@@ -6,9 +6,12 @@
 namespace PosInformatique.MSBuild.Yaml
 {
     using System;
+    using System.Globalization;
     using System.IO;
     using Microsoft.Build.Framework;
     using Newtonsoft.Json;
+    using YamlDotNet.Core;
+    using YamlDotNet.Core.Events;
     using YamlDotNet.Serialization;
 
     /// <summary>
@@ -16,6 +19,12 @@ namespace PosInformatique.MSBuild.Yaml
     /// </summary>
     public sealed class YamlToJson : Microsoft.Build.Utilities.Task
     {
+        /// <summary>
+        /// YAML deserializer.
+        /// </summary>
+        private static readonly IDeserializer yamlDeserializer = new DeserializerBuilder()
+            .WithNodeDeserializer(new ScalarNodeDeserializer()).Build();
+
         /// <summary>
         /// Initializes a new instance of the <see cref="YamlToJson"/> class.
         /// </summary>
@@ -73,8 +82,6 @@ namespace PosInformatique.MSBuild.Yaml
                 this.Log.LogMessage(MessageImportance.Normal, MsBuildYamlResources.LogConvertingYamlFile, this.YamlInputFile, this.JsonOutputFile, MsBuildYamlResources.WithoutIndentation);
             }
 
-            var yamlDeserializer = new Deserializer();
-
             using (var inputReader = new StreamReader(this.YamlInputFile))
             {
                 var yamlContent = yamlDeserializer.Deserialize(inputReader);
@@ -93,6 +100,61 @@ namespace PosInformatique.MSBuild.Yaml
             }
 
             return true;
+        }
+
+        /// <summary>
+        /// Implementation of the <see cref="INodeDeserializer"/> which allows to
+        /// parse the scalar values:
+        /// - Boolean
+        /// - Integer
+        /// - Decimal
+        /// - Empty values to null
+        /// </summary>
+        private sealed class ScalarNodeDeserializer : INodeDeserializer
+        {
+            /// <summary>
+            /// <see cref="YamlDotNet.Serialization.NodeDeserializers.ScalarNodeDeserializer"/> used to retrieve
+            /// the default behavior for the not supported scalar values.
+            /// </summary>
+            private static readonly INodeDeserializer defaultNodeDeserializer = new YamlDotNet.Serialization.NodeDeserializers.ScalarNodeDeserializer();
+
+            /// <inheritdoc />
+            public bool Deserialize(IParser reader, Type expectedType, Func<IParser, Type, object> nestedObjectDeserializer, out object value)
+            {
+                if (reader.Accept(out Scalar scalar))
+                {
+                    if (string.IsNullOrEmpty(scalar.Value))
+                    {
+                        value = null;
+                        reader.MoveNext();
+
+                        return true;
+                    }
+                    else if (bool.TryParse(scalar.Value, out bool booleanValue))
+                    {
+                        value = booleanValue;
+                        reader.MoveNext();
+
+                        return true;
+                    }
+                    else if (int.TryParse(scalar.Value, NumberStyles.Any, CultureInfo.InvariantCulture, out int integerValue))
+                    {
+                        value = integerValue;
+                        reader.MoveNext();
+
+                        return true;
+                    }
+                    else if (decimal.TryParse(scalar.Value, NumberStyles.Any, CultureInfo.InvariantCulture , out decimal decimalValue))
+                    {
+                        value = decimalValue;
+                        reader.MoveNext();
+
+                        return true;
+                    }
+                }
+
+                return defaultNodeDeserializer.Deserialize(reader, expectedType, nestedObjectDeserializer, out value);
+            }
         }
     }
 }
